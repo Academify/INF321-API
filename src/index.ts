b537769208737d2d7e44337d3eaf74e442cf6cab
Thiago Ferreira Peixoto <thiago.ferreira@dtidigital.com.br>
2022-10-05 10:09:34 -0300

    Usando rotas

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(routes);
+app.use(express.json());
 
 app.listen(3000);
